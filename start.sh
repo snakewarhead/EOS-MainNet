caea6212945d94dfa2637131aeb8e8d069068a24
snakewarhead <snakewarhead@gmail.com>
2018-08-17 11:04:35 +0800

    memory must below 8g

--- start.sh
+++ start.sh
@@ -20,3 +20,4 @@ ulimit -n 65535
 ulimit -s 64000
 
 $NODEOSBINDIR/nodeos/nodeos --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+#$NODEOSBINDIR/nodeos/nodeos --data-dir $DATADIR --config-dir $DATADIR $@
